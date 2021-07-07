c1c7f6dea10c5ec3a918ba002a927d8e2ac571cd
Ivcast20 <ivan.castroa@ug.edu.ec>
2021-07-06 23:29:39 -0500

    Nueva nota de pedido con diseño agregado

--- app/src/main/java/com/example/proyecto_electiva_grupo5/NuevaNotaDePedido.java
+++ app/src/main/java/com/example/proyecto_electiva_grupo5/NuevaNotaDePedido.java
@@ -13,26 +13,9 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class NuevaNotaDePedido extends AppCompatActivity {
-    TextView textoFecha;
-    private ListView listaProductos;
-    private ArrayList<String> nombresProductos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_nota_de_pedido);
-        Date fechaActual = new Date();
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-        textoFecha = findViewById(R.id.txtFecha);
-        textoFecha.setText(formato.format(fechaActual));
-        listaProductos = (ListView) findViewById(R.id.listaProductos);
-        this.nombresProductos = new ArrayList<>();
-        this.nombresProductos.add("Zapatos de Hombre");
-        this.nombresProductos.add("Zapatos de Mujer");
-        this.nombresProductos.add("Blusa azul talla M");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombresProductos);
-        listaProductos.setAdapter(adapter);
     }
-
-
 }
