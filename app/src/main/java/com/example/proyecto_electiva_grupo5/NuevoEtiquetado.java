70aa6cdafc043303362fe174cf38dcc876fe6931
josemrx55 <jose-m-roldan-123@hotmail.com>
2021-07-16 08:55:19 -0500

    Añadí 5 activities y terminé 2 que les faltaban 5 elementos

---
+++ app/src/main/java/com/example/proyecto_electiva_grupo5/NuevoEtiquetado.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_electiva_grupo5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevoEtiquetado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_etiquetado);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto_electiva_grupo5/TrazabilidadProceso.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_electiva_grupo5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrazabilidadProceso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trazabilidad_proceso);
+    }
+}
